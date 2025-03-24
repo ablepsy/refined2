ee6ef16b79742693cc4ebfc62be356b80969c00a
ablepsy <sallaj@ablepsychiatry.com>
2025-03-23 20:43:22 -0500

    Delete chatgpt-finetuning-app-updated 2 directory

--- chatgpt-finetuning-app-updated 2/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // The app directory is no longer experimental in newer versions
-  // but keeping this more compatible with Next.js 13.4.19
-}
-
-module.exports = nextConfig
